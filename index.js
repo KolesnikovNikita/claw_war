314cac2d40587d53d456c3039b6cbb4f02d02c83
Nikita Kolesnikov <nik.kolesnikov.andrew@gmail.com>
2022-08-15 19:58:27 +0300

    password show/hide js function

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-// При нажатии стрелки вверх скролл в начало страницы
+// scroll up by click
 
 document.getElementById('scroll-up-arrow').onclick = function scrollUpPage() {
 	window.scrollTo(0, 0);
 };
+
+// 	show / hide password
+
+function initPasswordShow() {
+	const passwordInputEyes = document.querySelectorAll(
+		'[data-component=PasswordInputEye]'
+	);
+	passwordInputEyes.forEach(function (eyeButton) {
+		eyeButton.addEventListener('click', function () {
+			const passwordInputId = eyeButton.dataset.passwordInputId;
+
+			const passwordInput = document.getElementById(passwordInputId);
+
+			console.log(passwordInputId);
+
+			// if (passwordInput.getAttribute('type') === 'password') {
+			// 	passwordInput.setAttribute('type', 'text');
+			// } else {
+			// 	passwordInput.setAttribute('type', 'password');
+			// }
+		});
+	});
+}
+
+function initComponents() {
+	initPasswordShow();
+}
+
+initComponents();
